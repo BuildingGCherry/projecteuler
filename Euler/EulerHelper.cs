7ad4c24bc9d0e2f5d7f94bae1443c2ce94d00e84
gcherry <gcherry2@gmail.com>
2015-06-21 23:15:15 -0400

    Project Euler question 4 with comments

--- Euler/EulerHelper.cs
+++ Euler/EulerHelper.cs
@@ -1,10 +1,50 @@
-﻿namespace Euler
+﻿using System.Linq;
+
+namespace Euler
 {
     public class EulerHelper
     {
+        #region Methods
+
+        public long GetLargestPrimeNumber(long number)
+        {
+            var newNum = number;
+            long largestFactor = 0;
+            long count = 2;
+
+            while (count*count <= newNum)
+            {
+                if (newNum%count == 0)
+                {
+                    newNum = newNum/count;
+                    largestFactor = count;
+                }
+                else
+                {
+                    count = (count == 2) ? 3 : count + 2;
+                }
+            }
+            if (newNum > largestFactor)
+            {
+                // the remainder is a prime number
+                largestFactor = newNum;
+            }
+
+            return largestFactor;
+        }
+
+        public bool IsPalindrome(long number)
+        {
+            var s = number.ToString();
+
+            ///this can be done very easy with the use of linq
+            return s.Reverse().SequenceEqual(s);
+
+        }
+
         public bool IsPrimeNumber(long number)
         {
-            for (int i = 2; i <= (number/2); i++)
+            for (var i = 2; i <= (number/2); i++)
             {
                 if (number%i == 0)
                 {
@@ -14,30 +54,6 @@
             return true;
         }
 
-        public long GetLargestPrimeNumber(long number)
-        {
-            long newNum = number;
-            long largestFactor = 0;
-            long count = 2;
-
-            while (count * count <= newNum)
-            {
-                if (newNum % count == 0)
-                {
-                    newNum = newNum / count;
-                    largestFactor = count;
-                }
-                else
-                {
-                    count = (count == 2) ? 3 : count + 2;
-                }
-            }
-            if (newNum > largestFactor)
-            { // the remainder is a prime number
-                largestFactor = newNum;     
-            }
-
-            return largestFactor;
-        }
+        #endregion
     }
 }
--- Euler/Form1.cs
+++ Euler/Form1.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Globalization;
+using System.Linq;
 using System.Windows.Forms;
 
 #endregion
@@ -10,7 +11,11 @@ namespace Euler
 {
     public partial class Form1 : Form
     {
+        #region Fields
+
         private readonly EulerHelper _eulerHelper = new EulerHelper();
+
+        #endregion
 
         #region Constructors
 
@@ -23,12 +28,17 @@ namespace Euler
 
         #region Methods
 
+        /// <summary>
+        /// Project euler problem 1
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnQ1_Click(object sender, EventArgs e)
         {
-            int result = 0;
-            int sum = 0;
+            var result = 0;
+            var sum = 0;
 
-            for (int i = 0; i < 1000; i++)
+            for (var i = 0; i < 1000; i++)
             {
                 if (i%3 == 0 && i%5 == 0)
                 {
@@ -50,12 +60,17 @@ namespace Euler
             lblAnswers.Text = result.ToString(CultureInfo.InvariantCulture);
         }
 
+        /// <summary>
+        /// Project euler problem 2
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnQ2_Click(object sender, EventArgs e)
         {
-            int fibPrevious = 0;
-            int fibCurrent = 1;
-            int fibNext = 0;
-            int totalSum = 0;
+            var fibPrevious = 0;
+            var fibCurrent = 1;
+            var fibNext = 0;
+            var totalSum = 0;
 
             do
             {
@@ -74,17 +89,56 @@ namespace Euler
             lblAnswers.Text = totalSum.ToString();
         }
 
+        /// <summary>
+        /// Project euler problem 3
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnQ3_Click(object sender, EventArgs e)
         {
-            long maxNumber = 600851475143;
+            var maxNumber = 600851475143;
 
             lblAnswers.Text = _eulerHelper.GetLargestPrimeNumber(maxNumber).ToString();
-           
+
             lblQuestions.Text =
                 "The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime factor of the number 600851475143. " +
                 "(A Prime Number can be divided evenly only by 1, or itself. And it must be a whole number greater than 1. " +
                 "Example: 5 can only be divided evenly by 1 or 5, so it is a prime number. But 6 can be divided evenly by 1, 2, 3 and 6 so it is NOT a prime number " +
                 "(it is a composite number)";
+        }
+
+        /// <summary>
+        /// Project euler problem 4
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnQ4_Click(object sender, EventArgs e)
+        {
+            lblQuestions.Text =
+                "A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is " +
+                "9009 = 91 × 99.Find the largest palindrome made from the product of two 3-digit numbers.";
+
+            ///Found that using linq makes this a bit easier
+            var results = Enumerable.Range(100, 900).
+                SelectMany(x => Enumerable.Range(x, 1000 - x).Select(y => x*y)).
+                Where(z => _eulerHelper.IsPalindrome(z)).Max();
+
+            lblAnswers.Text = results.ToString();
+
+            ///Alternate solution using for loops
+            //var results = new List<int>();
+            //for (var x = 100; x < 1000; ++x)
+            //{
+            //    for (var y = x; y < 1000; ++y)
+            //    {
+            //        if (_eulerHelper.IsPalindrome(x * y))
+            //        {
+            //            results.Add(x * y);
+            //        }
+            //    }
+            //}
+
+            //lblAnswers.Text = results.Select(x => x).Where(y => _eulerHelper.IsPalindrome(y)).Max().ToString();
         }
 
         #endregion
